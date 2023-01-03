ee1b6ee66f28c5b8977ead4fe40901deb03b989a
Kevin Rodrigues <kevin.desenv@gmail.com>
2023-01-03 18:29:17 +0000

    mudança que esqueci

--- src/app/services/http-check.service.ts
+++ src/app/services/http-check.service.ts
@@ -9,19 +9,6 @@ export class HttpCheckService {
 
   constructor(private http: HttpClient) { }
 
-  // public isHostAvaliable(hostname:string): boolean{
-  //   var flag = false
-  //   lookup(hostname, (err, address, family) => {
-  //     if (err) {
-  //       console.error(`Error: ${err.message}`);
-  //     } else {
-  //       console.log(`Host ${hostname} is available at ${address}`);
-  //       flag = true;
-  //     }
-  //   });
-  //   return flag;
-  // }
-
   isHostAvaliable(item: IotService) {
     this.http.get(item.host).subscribe(
       res => {
