8f6116dbd9038b6d90687a9b23742c86ee74ea95
Kevin Rodrigues <kevin.desenv@gmail.com>
2023-01-03 19:24:10 +0000

    trying to allow CORS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,10 @@ export class AppComponent {
   constructor(httpCheckService:HttpCheckService){
     this.httpCheckService= httpCheckService;
     this.iotServiceList = [
-      {isAvaliable:false, name:"cam 1", uri:":81/stream", host: "http://192.168.1.82"},
-      {isAvaliable:false, name:"cam 2", uri:":81/stream", host: "http://192.168.1.84"}
+      {isAvaliable:false, name:"cam 1", uri:":81/stream", host: "/cam1"},
+      // {isAvaliable:false, name:"cam 1", uri:":81/stream", host: "http://192.168.1.82"},
+      // {isAvaliable:false, name:"cam 2", uri:":81/stream", host: "http://192.168.1.84"}
+      {isAvaliable:false, name:"cam 2", uri:":81/stream", host: "/cam2"}
     ]
   }
 
